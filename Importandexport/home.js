20b2d1dc235934a0609fb25cb762b8b8b1d562cf
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-09-24 11:31:06 +0530

    react

---
+++ Importandexport/home.js
@@ -0,0 +1,7 @@
+let user="upadhyay"
+let firstName="astha"
+function sum(){
+    let a=9;
+    let b=8;
+    console.log(a+b)
+}
---
+++ Importandexport/index.js
@@ -0,0 +1,5 @@
+import a from './home.js'
+import { firstName,sum} from './home.js';
+console.log(a)
+console.log(firstName)
+sum()
